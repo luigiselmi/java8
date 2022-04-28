f6d9006e43593e92d3fbf596fcfeb26fdca76e43
Luigi Selmi <luigiselmi@gmail.com>
2022-04-28 17:51:52 +0200

    init test 4

---
+++ src/main/java/hr/test4/Result.java
@@ -0,0 +1,14 @@
+package hr.test4;
+
+import java.util.List;
+
+public class Result {
+  /*
+   * The function is expected to return an integer.
+   * The function accepts string array.
+   */
+  public static int countGroups(List<String> related) {
+    return -1;
+  }
+
+}
---
+++ src/main/java/hr/test4/Solution.java
@@ -0,0 +1,36 @@
+package hr.test4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    
+    int relatedCount = Integer.parseInt(bufferedReader.readLine().trim());
+    
+    List<String> related = IntStream.range(0, relatedCount).mapToObj(i -> {
+      try {
+        return bufferedReader.readLine().replaceAll("\\s+$", "");
+      }
+      catch (IOException ioe) {
+        throw new RuntimeException(ioe);
+      }
+    })
+    .collect(Collectors.toList());
+    
+    int result = Result.countGroups(related);
+    
+    bufferedReader.close();
+    
+    System.out.println("Related = " + result);
+  
+
+  }
+
+}
