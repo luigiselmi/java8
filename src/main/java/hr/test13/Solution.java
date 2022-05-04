95ba71fb8a15fe449a8ab10fe2e33ae25f941163
Luigi Selmi <luigiselmi@gmail.com>
2022-05-04 21:37:14 +0200

    java stack

---
+++ src/main/java/hr/test13/Solution.java
@@ -0,0 +1,17 @@
+package hr.test13;
+
+import java.util.Scanner;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    while (sc.hasNext()) {
+      String input=sc.nextLine();
+            //Complete the code
+    }
+
+  }
+
+}
